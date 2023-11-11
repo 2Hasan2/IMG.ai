590f7d0a7f6810fd4a81c2439359590c4fa9549d
0Hasan0 <133760904+2Hasan2@users.noreply.github.com>
2023-11-11 23:30:44 +0200

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,22 @@
-const CACHE_NAME = 'IMG-ai-cache-v1';
+const CACHE_NAME = 'IMG-ai-cache-v2';
 const urlsToCache = [
     '/',
     '/style.css',
     '/script.js',
     '/AI.png',
 ];
-
-self.addEventListener('install', event => {
+self.addEventListener('load', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .catch(error => console.error('Cache addAll failed:', error))
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request)
-                    .then(response => {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // Clone the response as it can only be consumed once
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    })
-                    .catch(error => console.error('Fetch failed:', error));
-            })
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
