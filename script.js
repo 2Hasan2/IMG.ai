47f8d6f298be2a223c355f4e51ec1b5ad0252c00
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-12-14 21:26:31 +0200

    add style and num

--- script.js
+++ script.js
@@ -1,44 +1,77 @@
-const API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImE5MTcxZTUxZTViYmVjNGM5ZDUyMzg1OGI3Mzc1Zjk3IiwiY3JlYXRlZF9hdCI6IjIwMjMtMTItMTRUMTM6Mjc6NTQuNzk4MDE4In0.Qo6a7JkEsLlVIwN3WqQzLE4HpElq_BAdJB5tOsRHgqE';
-const URL = 'https://api.monsterapi.ai/v1/generate/sdxl-base';
 
-async function sendAndFetchResult(prompt) {
-    try {
-        const sendOptions = {
-            method: 'POST',
-            headers: {
-                accept: 'application/json',
-                'content-type': 'application/json',
-                authorization: `Bearer ${API_KEY}`
-            },
-            body: JSON.stringify({ safe_filter: false, prompt })
-        };
-
-        const sendResponse = await fetch(URL, sendOptions);
-        const sendResult = await sendResponse.json();
-
-        const getResult = async (process_id) => {
-            const getOptions = {
-                method: 'GET',
-                headers: {
-                    accept: 'application/json',
-                    authorization: `Bearer ${API_KEY}`
-                }
-            };
-
-            const getStatusResponse = await fetch(`https://api.monsterapi.ai/v1/status/${process_id}`, getOptions);
-            const statusResult = await getStatusResponse.json();
-
-            if (statusResult.status === 'IN_PROGRESS' || statusResult.status === 'IN_QUEUE') {
-                return new Promise(resolve => setTimeout(() => resolve(getResult(process_id)), 1000));
-            } else if (statusResult.status === 'COMPLETED') {
-                return statusResult.result.output;
-            } else {
-                throw new Error(JSON.stringify(statusResult));
-            }
-        };
-
-        return getResult(sendResult.process_id);
-    } catch (error) {
-        throw new Error(error);
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        Texting();
     }
+}
+
+
+function Texting() {
+    const chatMessages = document.getElementById('chat-messages');
+    const prompt = document.getElementById('textCommand').value || 'Generate Image';
+    const style = document.getElementById('styleSelect').value;
+    const samples = +document.getElementById('numberInput').value;
+
+    console.log(prompt, style, samples);
+    const options={
+        prompt,
+        style,
+        safe_filter: true,
+        samples
+    }
+    // handle empty option value use loop
+    for (const key in options) {
+        if (options[key] === '') {
+            delete options[key];
+        }
+    }
+    // Display user message
+    message(options , true);
+
+    // Display AI message
+    message(options , false);
+
+    document.getElementById('textCommand').value = '';
+
+    // Scroll to the bottom of the chat messages
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+
+function message(options, isUser) {
+    const chatMessages = document.getElementById('chat-messages');
+    const message = document.createElement('div');
+    // append loader
+    message.className = 'message-container';
+    message.innerHTML = `<div class="${isUser ? 'user-message loading-img' : 'ai-message'} card" onclick="copyToClipboard(this)">${isUser ? options.prompt : 'loading...'}</div>`;
+    chatMessages.appendChild(message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+    // if not user message, generate image
+    if (!isUser) {
+        sendAndFetchResult(options)
+            .then(result => {
+                // delete loading text
+                message.querySelector('.ai-message').innerHTML = '';
+                result.forEach(Url => {
+                    const outputImage = document.createElement('img');
+                    outputImage.src = Url;
+                    outputImage.alt = 'Generated Image';
+                    message.querySelector('.ai-message').appendChild(outputImage);
+                });
+            })
+            .catch(error => console.error(error));
+    };
+}
+
+
+
+function copyToClipboard(element) {
+    const textToCopy = element.innerText;
+    const tempInput = document.createElement('input');
+    document.body.appendChild(tempInput);
+    tempInput.value = textToCopy;
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
 }
