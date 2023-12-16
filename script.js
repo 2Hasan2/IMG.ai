cfe8697a16ba52182c7573e00a65e8a42e19e1c8
2Hasan2 <133760904+2Hasan2@users.noreply.github.com>
2023-12-16 21:33:24 +0200

    fix style and can generate 20 img

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const chunkSize = 4;
+const chatMessages = document.getElementById('chat-messages');
 
 function handleKeyPress(event) {
     if (event.key === 'Enter') {
@@ -6,72 +8,75 @@ function handleKeyPress(event) {
     }
 }
 
-
 function Texting() {
-    const chatMessages = document.getElementById('chat-messages');
     const prompt = document.getElementById('textCommand').value;
     const style = document.getElementById('styleSelect').value;
-    const samples = +document.getElementById('numberInput').value;
- if (prompt === '') {
+    const num_imgs = +document.getElementById('numberInput').value;
+    num_imgs = Math.min(num_imgs, 20);
+    if (prompt === '') {
         return;
     }
+    
 
-    const options={
-        prompt,
-        style,
-        safe_filter: false,
-        samples
-    }
-    // handle empty option value use loop
-    for (const key in options) {
-        if (options[key] === '') {
-            delete options[key];
+    userMessage(prompt);
+    
+    for (let i = 0; i < Math.ceil(num_imgs / chunkSize); i++) {
+        const options = {
+            prompt,
+            style,
+            safe_filter: false,
+            samples: Math.min(chunkSize, num_imgs - i * chunkSize)
+        };
+
+        // Remove empty options
+        for (const key in options) {
+            if (options[key] === '') {
+                delete options[key];
+            }
         }
+        aiMessage(options);
     }
-    // Display user message
-    message(options , true);
-
-    // Display AI message
-    message(options , false);
 
     document.getElementById('textCommand').value = '';
+}
+
+// Function for handling user messages
+function userMessage(prompt) {
+    const USER_MSG = document.createElement('div');
+    USER_MSG.className = 'user-message';
+    USER_MSG.innerHTML = prompt;
+    USER_MSG.setAttribute('onclick', 'copyToClipboard(this)');
+    chatMessages.appendChild(USER_MSG);
 
     // Scroll to the bottom of the chat messages
     chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
+async function aiMessage(options) {
+    const AI_MSG = document.createElement('div');
+    AI_MSG.className = 'ai-message ';
+    AI_MSG.innerHTML = 'loading...';
+    chatMessages.appendChild(AI_MSG);
 
-function message(options, isUser) {
-    const chatMessages = document.getElementById('chat-messages');
-    const message = document.createElement('div');
-    // append loader
-    message.className = 'message-container';
-    message.innerHTML = `<div class="${isUser ? 'user-message loading-img' : 'ai-message'} card" onclick="copyToClipboard(this)">${isUser ? options.prompt : 'loading...'}</div>`;
-    chatMessages.appendChild(message);
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-    // if not user message, generate image
-    if (!isUser) {
-        sendAndFetchResult(options)
-            .then(result => {
-                // delete loading text
-                message.querySelector('.ai-message').innerHTML = '';
-                result.forEach(Url => {
-                    const outputImage = document.createElement('img');
-                    outputImage.src = Url;
-                    outputImage.alt = 'Generated Image';
-                    message.querySelector('.ai-message').appendChild(outputImage);
-                });
-            })
-            .catch(error=> {
-                console.log(error);
-                // convert it to string
-                const errorMessage = JSON.parse(error.message).result.errorMessage;
-                message.querySelector('.ai-message').innerHTML = errorMessage;
-            });
-    };
+    try {
+        const result = await sendAndFetchResult(options);
+
+        AI_MSG.innerHTML = '';
+        result.forEach(url => {
+            const outputImage = document.createElement('img');
+            const card = document.createElement('div');
+            card.className = 'card';
+            outputImage.src = url;
+            outputImage.alt = 'Generated Image';
+            card.appendChild(outputImage);
+            AI_MSG.appendChild(card);
+        });
+    } catch (error) {
+        console.error(error);
+        const errorMessage = error.message ? JSON.parse(error.message).result.errorMessage : 'An error occurred';
+        AI_MSG.innerHTML = errorMessage;
+    }
 }
-
-
 
 function copyToClipboard(element) {
     const textToCopy = element.innerText;
